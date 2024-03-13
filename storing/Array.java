eb66f6478393f6975784a8626ae35808398d4848
gin7018 <79063763+gin7018@users.noreply.github.com>
2024-03-13 15:14:40 -0400

    add array store stub

---
+++ storing/Array.java
@@ -0,0 +1,21 @@
+// using an array to store the text file
+
+public class Array {
+    private ArrayList<String> store;
+    private int[] cursor;
+
+    public array() {
+        this.store = new ArrayList<>();
+        this.cursor = new int[2]{0, 0};
+
+    }
+
+    public void add(String text) {
+        // todo
+    }
+
+    public void delete(Integer row, Integer col) {
+        // todo
+    }
+
+}
